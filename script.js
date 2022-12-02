0e209e122a2d0f18dceaf499153739badc22e46b
dahye <dhk8233@gmail.com>
2022-12-02 10:58:26 +0900

    portfolio

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ console.log(emailCopy);
 emailCopy.addEventListener('click', ()=>{
   const emailText = emailCopy.textContent
   
-  navigator.clipboard.writeText(emailText).then(() => {
+  navigator.clipboard.writeText(emailText).then(
     alert('이메일 주소가 복사되었습니다.')
-  })
+  )
 
 })
